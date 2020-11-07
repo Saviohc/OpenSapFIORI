b4c1317eab2e11ffd9133b49f76f5ef68f5e05bf
Saviohc <saviohc@gmail.com>
2020-11-07 17:31:54 +0000

    Week 2

--- MyAppW2U1/webapp/Component.js
+++ MyAppW2U1/webapp/Component.js
@@ -14,6 +14,12 @@ sap.ui.define([
 			UIComponent.prototype.init.apply(this, arguments);
 
 			// additional initialization can be done here
+			 
+			//* test with languages 
+			// console.log(sap.ui.getCore().getConfiguration().getLanguage());
+			// sap.ui.getCore().getConfiguration().setLanguage("EN");
+			// console.log(sap.ui.getCore().getConfiguration().getLanguage());
+			//
 		}
 
 	});
