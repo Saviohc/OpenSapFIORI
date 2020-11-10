6db2bdaaf63e390b7850a5344114e8ee93314493
Saviohc <saviohc@gmail.com>
2020-11-10 16:47:46 +0000

    Week 6 and Bonus

---
+++ MyAPPW2U6_AND_BONUS/webapp/Component.js
@@ -0,0 +1,31 @@
+sap.ui.define([
+	"sap/ui/core/UIComponent"
+],  function(UIComponent) {
+	"use strict";
+
+	return UIComponent.extend("opensap.myapp.Component", {
+
+		metadata : {
+			manifest: "json"
+		},
+
+		init : function () {
+			// call the init function of the parent
+			UIComponent.prototype.init.apply(this, arguments);
+
+
+
+			// used only for this lessons to show the request individually...
+			this.getModel().setUseBatch(false);
+
+			// additional initialization can be done here
+			 
+			//* test with languages 
+			// console.log(sap.ui.getCore().getConfiguration().getLanguage());
+			// sap.ui.getCore().getConfiguration().setLanguage("EN");
+			// console.log(sap.ui.getCore().getConfiguration().getLanguage());
+			//
+		}
+
+	});
+});
